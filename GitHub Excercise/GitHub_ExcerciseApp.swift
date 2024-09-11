4cfb5607ca0b04278a0ed7081abd84bd269c22fa
micahmarc <micahmarc16@gmail.com>
2024-09-10 21:14:23 -0500

    Initial Commit

---
+++ GitHub Excercise/GitHub_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcerciseApp.swift
+//  GitHub Excercise
+//
+//  Created by Micah Marcellus on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
